b4d0add86e2c58bdb739a09d7d5e089235b5fccb
gcormier9 <gcormier9@gmail.com>
2025-04-01 18:54:55 +0000

    update

--- src/DraggableOverlay.jsx
+++ src/DraggableOverlay.jsx
@@ -1,76 +1,36 @@
-import { DragOverlay, defaultDropAnimationSideEffects } from "@dnd-kit/core";
+import {
+  DragOverlay,
+  defaultDropAnimation,
+  defaultDropAnimationSideEffects,
+} from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 
 const dropAnimation = {
   keyframes({ transform }) {
     return [
-      { transform: CSS.Transform.toString(transform.initial) },
+      { opacity: 1, transform: CSS.Transform.toString(transform.initial) },
       {
+        opacity: 0,
         transform: CSS.Transform.toString({
           ...transform.final,
-          scaleX: 0.94,
-          scaleY: 0.94,
+          x: transform.final.x + 5,
+          y: transform.final.y + 5,
         }),
       },
     ];
   },
-
-  sideEffects({ active, dragOverlay }) {
-    active.node.style.opacity = "0";
-
-    const button = dragOverlay.node.querySelector("button");
-
-    if (button) {
-      button.animate(
-        [
-          {
-            boxShadow:
-              "-1px 0 15px 0 rgba(34, 33, 81, 0.01), 0px 15px 15px 0 rgba(34, 33, 81, 0.25)",
-          },
-          {
-            boxShadow:
-              "-1px 0 15px 0 rgba(34, 33, 81, 0), 0px 15px 15px 0 rgba(34, 33, 81, 0)",
-          },
-        ],
-        {
-          duration: 250,
-          easing: "ease",
-          fill: "forwards",
-        }
-      );
-    }
-
-    return () => {
-      active.node.style.opacity = "";
-    };
-  },
-};
-
-const dropAnimation2 = {
-  duration: 250,
-  easing: "ease",
-  //keyframes: defaultDropAnimationKeyframes,
-  sideEffects: defaultDropAnimationSideEffects({
-    styles: {
-      active: {
-        opacity: "0",
-      },
-    },
-  }),
-};
-
-const dropAnimation3 = {
-  keyframes({ transform }) {
-    return [
-      { opacity: 1, transform: CSS.Transform.toString(transform.initial) },
-      { opacity: 0, transform: CSS.Transform.toString(transform.final) },
-    ];
+  easing: "ease-out",
+  sideEffects({ active }) {
+    active.node.animate([{ opacity: 0 }, { opacity: 1 }], {
+      duration: defaultDropAnimation.duration,
+      easing: defaultDropAnimation.easing,
+    });
   },
 };
 
 const DraggableOverlay = ({ children }) => {
   return (
-    <DragOverlay style={{ width: "2.6em" }} dropAnimation={dropAnimation3}>
+    <DragOverlay style={{ width: "2.6em" }} dropAnimation={dropAnimation}>
       {children}
     </DragOverlay>
   );
